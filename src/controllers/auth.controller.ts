d3ce6b487054ea6baccf9ba042a6d131bd0c4846
Felipe Pacheco Schürmann <felipe.xirma@gmail.com>
2021-03-18 12:25:24 +0000

    routes created

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -13,6 +13,10 @@ class AuthController {
     public async profile( req: Request, res: Response): Promise<Response> {
         return res.json();
     }
+
+    public async activateUser( req: Request, res: Response): Promise<Response> {
+        return res.json();
+    }
 }
 
 export default new AuthController();
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import AdminController from '~/controllers/admin.controller';
+import UserController from '~/controllers/user.controller';
 import AuthController from '../controllers/auth.controller';
 
 const router = Router();
@@ -7,9 +9,28 @@ const router = Router();
 router.post('/api/auth/login', AuthController.login);
 router.post('/api/auth/register', AuthController.signUp);
 router.get('/api/auth/profile', AuthController.profile);
+router.get('/api/auth/email/:user_id/activate', AuthController.activateUser);
 
 //Users
+router.get('/api/events', UserController.activeEvents);
+router.get('/api/projects', UserController.userProjects);
+router.post('/api/projects/create', UserController.createProject);
+router.put('/api/projects/update', UserController.updateProject);
+router.get('/api/projects/:project_id/detail', UserController.projectDetail);
+router.post('/api/events/inscription', UserController.eventInscription);
 
 //Admin
+router.get('/api/admin/projects', AdminController.allProjects);
+router.get('/api/admin/:event_id/projects', AdminController.projectsByEvent);
+router.get('/api/admin/:user_id/projects', AdminController.projectsByUser);
+router.delete('/api/admin/projects/:project_id/delete', AdminController.deleteProject);
+router.get('/api/admin/events', AdminController.allEvents);
+router.get('/api/admin/events/:event_id/detail', AdminController.eventDetail);
+router.post('/api/admin/events/create', AdminController.createEvent);
+router.put('/api/admin/events/update', AdminController.updateEvent);
+router.delete('/api/admin/events/:event_id/delete', AdminController.deleteProject);
+router.get('/api/admin/users', AdminController.allUsers);
+router.put('/api/admin/:user_id/update', AdminController.updateUser);
+router.delete('/api/admin/:user_id/delete', AdminController.deleteUser);
 
 export default router;
